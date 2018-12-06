9041c7fa547a4feb306198fbd4d94973d0fea1c5
Joe Saraceno <josephasaraceno@gmail.com>
2018-12-06 08:07:04 -0800

    just use webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./sass/main.scss"
+// import Background from './background.png';
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: "source-map",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, 
+          {
+            loader: "css-loader", 
+            options: {
+              sourceMap: true
+            }
+          }, 
+          {
+            loader: "sass-loader", 
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
